c090b9022f2cee7d88a041dd20728122f1a5634f
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-01-09 19:34:25 +0530

    9/1/24

--- Maximum Score After Splitting a String_1422.cpp
+++ Maximum Score After Splitting a String_1422.cpp
@@ -65,3 +65,23 @@ public:
 */
 
 
+class Solution {
+public:
+    int maxScore(string s) {
+        int n=s.length();
+        int ones=count(s.begin(),s.end(),'1');
+
+        int zeros=0;
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+            if(s[i]=='1'){
+                ones--;
+            }
+            else{
+                zeros++;
+            }
+            ans=max(ans,zeros+ones);
+        }
+        return ans;
+    }
+};
