c579157c7f7d7e0415850b5b2f1d0cd2f777c287
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-01-10 23:55:01 +0530

    ye

--- Binary Tree Preorder Traversal_144.cpp
+++ Binary Tree Preorder Traversal_144.cpp
@@ -1,7 +1,7 @@
 /*
 -------10/1/24-------
 
-Given the root of a binary tree, return the inorder traversal of its nodes' values.
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
 
  
 
@@ -9,7 +9,7 @@ Example 1:
 
 
 Input: root = [1,null,2,3]
-Output: [1,3,2]
+Output: [1,2,3]
 Example 2:
 
 Input: root = []
@@ -29,6 +29,41 @@ The number of nodes in the tree is in the range [0, 100].
 Follow up: Recursive solution is trivial, could you do it iteratively?
 */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//ITERATIVE SOLUTION
+/*
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> stk;
+        vector<int> ans;
+        TreeNode* curr=root;
+        while(curr!=NULL or !stk.empty()){
+            while(curr!=NULL){
+                stk.push(curr);
+                ans.push_back(curr->val);
+                curr=curr->left;
+            }
+            curr=stk.top();
+            stk.pop();
+            curr=curr->right;
+        }
+        return ans;
+    }
+    
+};
+*/
 
 /**
  * Definition for a binary tree node.
@@ -41,55 +76,22 @@ Follow up: Recursive solution is trivial, could you do it iteratively?
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-/*
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> seq;
-        inorder(root, seq);
-        return seq;
-    }
-
-    void inorder(TreeNode* root, vector<int>& nodes){
-        if(!root) return;
-        inorder(root->left, nodes);
-        nodes.push_back(root->val);
-        inorder(root->right, nodes);
-    }
-};
-
-*/
-
-/*
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
-//ITERATIVE SOLUTION BELOW
-class Solution {
-public:
-    vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        stack<TreeNode*> stk;
-        TreeNode* curr=root;
-        while(curr!=NULL or !stk.empty()){
-            while(curr!=NULL){
-                stk.push(curr);
-                curr=curr->left;
-            }
-            curr=stk.top();
-            stk.pop();
-            ans.push_back(curr->val);
-            curr=curr->right;
-
-        }
+        preorder(root, ans);
         return ans;
     }
+
+    void preorder(TreeNode* root, vector<int>& nodes){
+        if(!root) return;
+        if(root){
+            nodes.push_back(root->val);
+            preorder(root->left, nodes);
+            preorder(root->right, nodes);
+        }
+    }
+
 };
+
