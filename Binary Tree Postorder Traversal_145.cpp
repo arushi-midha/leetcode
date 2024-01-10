f04b91aeb0278f392a707bab3422bd89a77c541e
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-01-11 00:40:15 +0530

    Update Binary Tree Postorder Traversal_145.cpp

--- Binary Tree Postorder Traversal_145.cpp
+++ Binary Tree Postorder Traversal_145.cpp
@@ -28,3 +28,36 @@ The number of the nodes in the tree is in the range [0, 100].
 
 Follow up: Recursive solution is trivial, could you do it iteratively?
 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        postorder(root, ans);
+        return ans;
+    }
+
+    void postorder(TreeNode* node, vector<int>& ans){
+        if(!node) return;
+        postorder(node->left, ans);
+        postorder(node->right, ans);
+        ans.push_back(node->val);
+    }
+};
+
+/*
+ITERATIVE SOLUTION
+
+
+*/
