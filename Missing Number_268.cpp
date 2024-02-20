79a865dc6dd46bb4fbcd31f67889dd3ae917e3f6
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-02-21 00:36:57 +0530

    added Missing Number_268

--- Missing Number_268.cpp
+++ Missing Number_268.cpp
@@ -57,3 +57,40 @@ public:
       return miss;
     }
 };
+
+/*
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> arr(n+1);
+        int miss;
+        for(int i=0;i<n;i++){
+            arr[nums[i]]=1;
+        }
+        for(int i=0;i<n+1;i++){
+            if(arr[i]!=1){
+                miss=i;
+            }
+        }
+        return miss;
+    }
+};
+*/
+
+
+/*
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int total=(n*(n+1))/2;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+
+        return total-sum;
+    }
+};
+*/
