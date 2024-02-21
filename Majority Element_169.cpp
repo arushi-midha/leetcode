f82b37bfce7b6134da1038d49f4dc7338bea34ff
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-02-21 10:39:57 +0530

    updated majority element

--- Majority Element_169.cpp
+++ Majority Element_169.cpp
@@ -45,3 +45,38 @@ public:
         return element;
     }
 };
+
+/*
+less optimal solution
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
+    }
+};
+*/
+
+/*
+---------------USING MOORE VOTING ALGORITHM----------------
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int candidate=0;
+        for(int num:nums){
+            if(count==0){
+                candidate=num;
+            }
+
+            if(num==candidate){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return candidate;
+    }
+};
+*/
