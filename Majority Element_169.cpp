1cfc7b01be4f40009a4e32ca7e57091e2cbda818
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-02-21 09:35:47 +0530

    added majority element

---
+++ Majority Element_169.cpp
@@ -0,0 +1,47 @@
+/*
+21/2/24
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+ 
+
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> mymap;
+        for(int i=0;i<nums.size();i++){
+            mymap[nums[i]]++;
+        }
+        int max_count=0;
+        int element=-1;
+        for(auto x:mymap){
+            if(x.second>max_count){
+                max_count=x.second;
+                element=x.first;
+            }
+        }
+        return element;
+    }
+};
