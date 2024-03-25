23b69b527e235db47620e16f323ac3d97aef67ea
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-03-25 20:07:58 +0530

    25 march daily

--- Find the Duplicate Number_287.cpp
+++ Find the Duplicate Number_287.cpp
@@ -61,3 +61,27 @@ public:
         return ans;
     }
 };
+
+
+//---------------------BETTER SOLUTION
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow=0;
+        int fast=0;
+        while (true){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            if(slow==fast){
+                break;
+            }
+        }
+        int curr=0;
+        while(curr!=slow){
+            curr=nums[curr];
+            slow=nums[slow];
+        }
+        return curr;
+    }
+};
