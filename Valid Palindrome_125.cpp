0fef0b68426abac7940a0ce11e3fd6c2c374b60e
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-03-27 23:03:20 +0530

    27 march daily

--- Valid Palindrome_125.cpp
+++ Valid Palindrome_125.cpp
@@ -61,3 +61,36 @@ public:
         return true;
     }
 };
+
+
+/*
+beats 80%
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n=s.size();
+        int i=0;
+        int j=n-1;
+    
+        while(i<=j){
+            if(!isalnum(s[i])){
+                i++;
+                continue;
+            }
+            if(!isalnum(s[j])){
+                j--;
+                continue;
+            }
+            if(tolower(s[i])!=tolower(s[j])){
+                    return false;
+            }
+            else{
+            i++;
+            j--;
+            }  
+        }
+        return true;
+    }
+};
+*/
