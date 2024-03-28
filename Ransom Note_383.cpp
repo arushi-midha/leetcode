b2ffcc5a8b38bc6c85e38770358a56cd41736c1e
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-03-29 00:10:54 +0530

    time optimized soln

--- Ransom Note_383.cpp
+++ Ransom Note_383.cpp
@@ -50,3 +50,25 @@ public:
 
     }
 };
+
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> m;
+
+        for(int i=0;i<magazine.length();i++){
+            m[magazine[i]]++;
+        }
+
+        for(int i=0;i<ransomNote.length();i++){
+            if(m[ransomNote[i]]>0){
+                m[ransomNote[i]]--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
