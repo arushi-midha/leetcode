fa63d54119c3f1d8cdb4d5b4bba20264b605e778
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-03-28 23:46:53 +0530

    two sum II

--- Two Sum_1.cpp
+++ Two Sum_1.cpp
@@ -48,3 +48,24 @@ public:
         return {};
     }
 };
+
+
+//OPTIMIZED
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]=i;
+        }
+
+        for(int i=0;i<nums.size();i++){
+            int value=target-nums[i];
+            if(m.count(value) && m[value]!=i){
+                return {i,m[value]};
+            }
+        }
+        return {};
+    }
+};
