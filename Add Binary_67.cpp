9457ce9361760ec65e47057ac6f7b4f4aa241d94
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-05-30 12:53:29 +0530

    30 may

---
+++ Add Binary_67.cpp
@@ -0,0 +1,49 @@
+/*
+29-5-23
+Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+ 
+
+Constraints:
+
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+*/
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i=a.length()-1;
+        int j=b.length()-1;
+        string result;
+        int carry=0;
+        while(i>=0||j>=0||carry==1){
+            int sum=carry;
+            if(i>=0){
+                sum+=a[i]-'0';
+                i--;
+            }
+            if(j>=0){
+                sum+=b[j]-'0';
+                j--;
+            }
+            carry=sum/2;
+            result+=((sum%2)+'0');
+
+        }
+        reverse(result.begin(),result.end());
+        return result;
+        
+    }
+};
