ba319b75b8a7646ccbddce1ce3f25b679ebb5206
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-06-07 20:49:18 +0530

    pascal triangle

---
+++ Hand of Straights_846.cpp
@@ -0,0 +1,59 @@
+/*
+6/6/24
+Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize, and consists of groupSize consecutive cards.
+
+Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, return true if she can rearrange the cards, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
+Output: true
+Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8]
+Example 2:
+
+Input: hand = [1,2,3,4,5], groupSize = 4
+Output: false
+Explanation: Alice's hand can not be rearranged into groups of 4.
+
+ 
+
+Constraints:
+
+1 <= hand.length <= 104
+0 <= hand[i] <= 109
+1 <= groupSize <= hand.length
+ 
+
+Note: This question is the same as 1296: https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/
+*/
+
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        map<int,int> freq;
+        if(hand.size()%groupSize!=0){
+            return false;
+        }
+        for(int i=0;i<hand.size();i++){
+            freq[hand[i]]++;
+        }
+        
+        while(!freq.empty()){
+            int card=freq.begin()->first;
+
+            for(int i=0;i<groupSize;i++){
+                if(freq[card+i]==0){
+                    return false;
+                }
+                freq[card+i]--;
+                if(freq[card+i]<1){
+                    freq.erase(card+i);
+                }
+            }
+        }
+
+        return true;
+    }
+};
