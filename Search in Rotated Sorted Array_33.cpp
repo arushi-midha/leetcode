e263255b66a8d332784e2e6b6840b3ae3ba10a7d
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-06-22 03:07:38 +0530

    21 june

--- Search in Rotated Sorted Array_33.cpp
+++ Search in Rotated Sorted Array_33.cpp
@@ -94,3 +94,16 @@ public:
         
     }
 };
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int ans=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return ans;
+    }
+};
